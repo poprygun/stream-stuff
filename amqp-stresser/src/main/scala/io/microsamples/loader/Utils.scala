d5a035d8f92f03f166817a7d57308ad3412ce76a
Alex Shumilov <ashumilov@pivotal.io>
2019-11-01 18:26:55 -0400

    Remove amqp dependencies from Kafka branch.

--- amqp-stresser/src/main/scala/io/microsamples/loader/Utils.scala
+++
@@ -1,9 +0,0 @@
-package io.microsamples.loader
-
-import io.gatling.core.feeder.Feeder
-
-object Utils {
-  private val counter = Iterator.from(1)
-
-  val idFeeder:Feeder[Int] = Iterator.continually(Map("id" -> counter.next()))
-}
